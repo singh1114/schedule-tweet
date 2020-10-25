59b580d2b0aee270177d57a2608e052b43253510
Ranvir Singh <ranvir.singh1114@gmail.com>
2020-10-26 00:58:14 +0530

    Add requirements

--- scheduletweet/twitter/scheduler.py
+++ scheduletweet/twitter/scheduler.py
@@ -5,5 +5,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(
-        tweet_scheduler, 'interval', minutes=1)
+        tweet_scheduler, 'interval', minutes=5)
     scheduler.start()
