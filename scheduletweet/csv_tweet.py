dd97f7e2623119a0b9cf5062d6640e835e8ed95b
Ranvir Singh <ranvir.singh1114@gmail.com>
2020-10-29 00:19:18 +0530

    Add csv code

--- scheduletweet/csv_tweet.py
+++ scheduletweet/csv_tweet.py
@@ -1,11 +1,10 @@
 import logging
 import arrow
-import os
 import tweepy
 import pandas as pd
+from apscheduler.schedulers.background import BlockingScheduler
 
-# from twitter.models import TwitterSchedulerModel
-
+logging.basicConfig(filename='./SchedulerLog.txt', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -13,16 +12,12 @@ def send_tweets(consumer_key, consumer_secret, access_token,
                 access_token_secret, tweet):
     # expired_tweets = TwitterSchedulerModel.objects.filter(
     #     sent=False, tweet_at__lte=arrow.utcnow().datetime)
-    expired_tweets = get_non_sent_tweets()
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    if expired_tweets.count() == 0:
-        logging.info("No tweet to send as of now")
-    for db_tweet in expired_tweets:
-        logging.info('sending tweet')
-        api.update_status(tweet)
-        logging.info('tweet sent')
+    logging.info('sending tweet')
+    api.update_status(tweet)
+    logging.info('tweet sent')
 
 
 def switch_to_arrow(df):
@@ -34,11 +29,13 @@ def get_non_sent_tweets(consumer_key, consumer_secret, access_token, access_toke
     df = pd.read_csv('tweets.csv')
     new_df = df.apply(switch_to_arrow, axis=1)
     applicable_rows = new_df[(new_df['sent']==False) & (new_df['tweet_at'] < arrow.utcnow())]
+    if len(applicable_rows) == 0:
+        return
     for index, data in applicable_rows.iterrows():
         send_tweets(
             consumer_key, consumer_secret, access_token, access_token_secret, data['tweet'])
         new_df.loc[new_df['sno'] == data['sno'], 'sent'] = True
-    new_df.to_csv()
+    new_df.to_csv('tweets.csv', index=False)
 
 
 def tweet_scheduler():
@@ -51,3 +48,10 @@ def tweet_scheduler():
         get_non_sent_tweets(consumer_key, consumer_secret, access_token, access_token_secret)
     else:
         raise NotImplementedError('Set environment variables correctly')
+
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+    scheduler.add_job(
+        tweet_scheduler, 'interval', minutes=5)
+    logger.info('starting scheduler')
+    scheduler.start()
